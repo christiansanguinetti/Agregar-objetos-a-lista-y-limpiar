06a072eefb95988fe4cde74a30907d4233541d92
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-24 19:53:06 -0300

    Update almacenar.js

--- js/almacenar.js
+++ js/almacenar.js
@@ -2,19 +2,35 @@ const nuevoitem = document.getElementById('item');
 const boton = document.getElementById('agregar');
 const conteniner = document.getElementById('contenedor');
 const borrar = document.getElementById('limpiar');
-let almacenamiento = [];
 
-function agregar(){
- if (nuevoitem.value) almacenamiento.push(nuevoitem.value)
- localStorage.setItem('almacenar',JSON.stringify(almacenamiento));
- almacenamiento = JSON.parse(localStorage.getItem('almacenar'))
+
+let almacenamiento = [];
+if (localStorage.getItem('almacenar')){
+    almacenamiento = JSON.parse(localStorage.getItem('almacenar'))
 }
-function mostrar (){
- conteniner.innerHTML += '<li>' + almacenamiento + '</li>';
+
+
+
+function agregar() {
+    if (nuevoitem.value) almacenamiento.push(nuevoitem.value)
+    localStorage.setItem('almacenar', JSON.stringify(almacenamiento));
+    almacenamiento = JSON.parse(localStorage.getItem('almacenar'))
 }
-boton.addEventListener ('click', ()=>{
-  agregar();
-  mostrar();
+function mostrar() {
+
+    conteniner.innerHTML += '<li>' + almacenamiento[almacenamiento.length - 1] + '</li>';
+}
+boton.addEventListener('click', () => {
+    agregar();
+    mostrar();
+    nuevoitem.value = ""
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+   for (elemento of JSON.parse(localStorage.getItem('almacenar'))) {
+        conteniner.innerHTML += '<li>' + elemento + '</li>';
+    }
+}) 
+
 
 
