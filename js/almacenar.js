e17f57744d31f4f9f4934bca7434126de661d807
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-24 18:50:10 -0300

    Update almacenar.js

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,4 +1,19 @@
-const nuevoitem = document.getElementById('item')
-const boton = document.getElementById('agregar')
+const nuevoitem = document.getElementById('item');
+const boton = document.getElementById('agregar');
+const conteniner = document.getElementById('contenedor');
+const borrar = document.getElementById('limpiar');
+let almacenamiento;
+
+function agregar(){
+ if (nuevoitem.value) localStorage.setItem('almacenar',nuevoitem.value);
+ almacenamiento = localStorage.getItem('almacenar')
+}
+function mostrar (){
+ conteniner.innerHTML += '<li>' + almacenamiento + '</li>';
+}
+boton.addEventListener ('click', ()=>{
+  agregar();
+  mostrar();    
+} )
 
 
