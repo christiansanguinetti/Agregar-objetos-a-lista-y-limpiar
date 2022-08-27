3794bc25fcdb049931319268ee1976102b2aaf00
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.2>
2022-08-27 00:21:01 -0300

    Acomode el codigo en una funcion

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,36 +1,35 @@
-const nuevoitem = document.getElementById('item');
-const boton = document.getElementById('agregar');
-const conteniner = document.getElementById('contenedor');
-const borrar = document.getElementById('limpiar');
+function almacenamientoLocal() {
+  const nuevoitem = document.getElementById("item");
+  const boton = document.getElementById("agregar");
+  const conteniner = document.getElementById("contenedor");
+  const borrar = document.getElementById("limpiar");
 
+  let almacenamiento = [];
+  if (localStorage.getItem("almacenar")) {
+    almacenamiento = JSON.parse(localStorage.getItem("almacenar"));
+  }
 
-let almacenamiento = [];
-if (localStorage.getItem('almacenar')){
-    almacenamiento = JSON.parse(localStorage.getItem('almacenar'))
-}
+  function agregar() {
+    if (nuevoitem.value) almacenamiento.push(nuevoitem.value);
+    localStorage.setItem("almacenar", JSON.stringify(almacenamiento));
+    almacenamiento = JSON.parse(localStorage.getItem("almacenar"));
+  }
+  function mostrar() {
+    conteniner.innerHTML +=
+      "<li>" + almacenamiento[almacenamiento.length - 1] + "</li>";
+  }
 
-
-
-function agregar() {
-    if (nuevoitem.value) almacenamiento.push(nuevoitem.value)
-    localStorage.setItem('almacenar', JSON.stringify(almacenamiento));
-    almacenamiento = JSON.parse(localStorage.getItem('almacenar'))
-}
-function mostrar() {
-
-    conteniner.innerHTML += '<li>' + almacenamiento[almacenamiento.length - 1] + '</li>';
-}
-boton.addEventListener('click', () => {
+  boton.addEventListener("click", () => {
     agregar();
     mostrar();
-    nuevoitem.value = ""
-})
+    nuevoitem.value = "";
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
-   for (elemento of JSON.parse(localStorage.getItem('almacenar'))) {
-        conteniner.innerHTML += '<li>' + elemento + '</li>';
+  document.addEventListener("DOMContentLoaded", () => {    
+    for (elemento of JSON.parse(localStorage.getItem("almacenar"))) {
+      conteniner.innerHTML += "<li>" + elemento + "</li>";
     }
-}) 
+  });
+}
 
-
-
+almacenamientoLocal();
